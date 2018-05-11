e154fa4bb6f4d7b21c4978ed843086f8ba77fc4c
Mauricio Kruijer <mauricio.kruijer@lightspeedhq.com>
2018-05-11 16:06:03 +0200

    Moved examples to its own folder
    Extracted from index.js

---
+++ examples/02-refresh-OTP.js
@@ -0,0 +1,19 @@
+const gaTOTP = require('../index');
+
+// This will generate a random base32 string with the default length of 16 characters
+const secret = gaTOTP.randomBase32();
+
+// The library is hardcoded to refesh the OTP every 30 seconds, this will calculate the time before the code expires
+const secondsLeft = 30 - Math.round(new Date() / 1000) % 30;
+
+/**
+ * Display in console the 6 digits code every time it changes
+ */
+console.log(`Your random secret is: ${secret} and the current code is ${gaTOTP.getOTP(secret)} and will expire in ${secondsLeft}s`);
+
+function refreshOTP() {
+	if (Math.round(new Date().getTime() / 1000) % 30 === 0) {
+		console.log(`New code ${gaTOTP.getOTP(secret)} will expire in 30s`);
+	}
+}
+setInterval(() => refreshOTP(), 1000);
---
+++ examples/03-serve-QR-code.js
@@ -0,0 +1,25 @@
+const gaTOTP = require('../index');
+
+/**
+ * Using the Google chart API to generate a QR code containing the secret
+ */
+const secret = gaTOTP.randomBase32();
+const user = 'gfiocco';
+const domain = 'github.com/gfiocco';
+const size = '300x300';
+const googleApisChartUrl = `https://chart.googleapis.com/chart?chs=${size}&chld=M|0&cht=qr&chl=otpauth://totp/${user}@${domain}%3Fsecret%3D${secret}`;
+
+/**
+ * Display the QR code in browser at http://localhost:3000
+ */
+var http = require('http');
+var server = http.createServer((req, res) => {
+	res.writeHead(301, {
+		'Cache-Control': 'no-cache', // prevent to reload old QR code when re-running
+		Location: googleApisChartUrl
+	});
+	res.end();
+});
+server.listen(3000);
+console.log('Open in your browser http://localhost:3000');
+console.log(gaTOTP.getOTP(secret));
